44c7630a1f8493a1e3e4da9e7375ada8f2140cf8
Brandon Roberts <broberts@pugetsound.edu>
2015-02-24 22:24:00 -0800

    Checks signup variables, adds functions for php, adds cookies, and starts a session upon login

--- php/signup.php
+++ php/signup.php
@@ -1,28 +1,16 @@
 <?php
 require_once 'connect.php';
+require_once 'functions.php';
 
-function NewUser($connection){
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	
-	$email = $_POST['eMail'];
-	
-	$password =  $_POST['password'];
-	
-	$pass = sha1($password, $raw_output = false);
-	
-	$sql= "INSERT INTO profile (f_Name,l_Name,email,pass) VALUES ('$firstName','$lastName','$email','$pass')";
-	$result = $connection->query($sql);
-	
-	header('Location: http://glados/capstone/index.html');		//redirect to the loginpage.html
+if(isset($_POST['submit'])){
+	SignUp($conn);
 }
 
 function SignUp($connection){
 	
 	if(!empty($_POST['firstName'])){  //checking the 'firstName' which is from register.html
 		//email from table and getting the POST[eMail] from register.html
-		
-		$sql = "SELECT * FROM profile WHERE email = '$_POST[eMail]' AND pass = '$_POST[password]'";
+		$sql = "SELECT * FROM profile WHERE email = '$_POST[eMail]' AND pass = '$_POST[password]'";//
 		$result = $connection->query($sql);
 	
 		if($result){
@@ -34,7 +22,48 @@ function SignUp($connection){
 	}
 }
 
-if(isset($_POST['submit'])){
-	SignUp($conn);
+function NewUser($connection){
+	//Set the error variables to empty
+	$firstNameErr = $lastNameErr = $emailErr = $passErr = "";
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		if(empty($_POST['firstName'])){
+			$firstNameErr = "First name is required.";
+		}else{ 
+			$firstName = test_Input($_POST['firstName']);
+			if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+				$firstNameErr = "Only letters and white space allowed";
+			}
+		}
+	
+		if(empty($_POST['lastName'])){
+			$lastNameErr = "Last name is required.";
+		}else{
+			$lastName = test_Input($_POST['lastName']);
+			if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+				$lastNameErr = "Only letters and white space allowed";
+			}
+		}
+	
+		if(empty($_POST['eMail'])){
+			$emailErr = "Email is required";
+		}else{
+			$email = test_Input($_POST['eMail']);
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "Invalid email format";
+			}
+		}
+	
+		if(empty($_POST['password'])){
+			$passErr = "Password is required";
+		}else{$password =  test_Input($_POST['password']);}
+	}
+	$pass = sha1($password, $raw_output = false);
+	
+	$sql= "INSERT INTO profile (f_Name,l_Name,email,pass) VALUES ('$firstName','$lastName','$email','$pass')";
+	$result = $connection->query($sql);
+	
+	header('Location: http://glados/capstone/index.html');		//redirect to the loginpage.html
 }
 ?>
--- php/userLogin.php
+++ php/userLogin.php
@@ -1,23 +1,30 @@
 <?php
-require_once connect.php;
+require_once 'connect.php';
 
-$email = $_POST['usermail'];
-$password = $_POST['password'];
-$pass = sha1($password, $raw_output = false); //get the encrypted password
+if(true){ //isset(_POST["submit"])  not working right now //if the Login button on index.html is set then do the logging in
+	logIn($conn);
+}
 
-LogIn($connection){
+function logIn($connection) {
+	//grab info from index.html 
+	$email = $_POST['usermail'];
+	$password = $_POST['password'];
+
+	$password = sha1($password, $raw_output = false); //get the encrypted password
 	
 	$sql = "SELECT pass FROM profile WHERE email = $email ";//encrypted password of the user
 	$result = $connection->query($sql);
 	
-	if($query == $pass){
-		
-	}
-	
-	
-}
+	if($result == $pass){//the user was indeed in the table and had the correct password
+		echo "Made it";
+		//start the session and initialize some cookies
+		session_start();
+        $_SESSION['email']= $_POST['userMail'];
+        $_SESSION['password']=$password; //save the encrypted version of their password
 
-if(isset($_POST['Login'])){ //if the Login button on index.html is set then do the logging in
-	LogIn($conn);
+		setcookie("email", $_SESSION['email'], time()+60*60*24);
+        setcookie("password", $_SESSION['password'], time()+60*60*24);
+		header("location:../profile.html");//go to the profile page for the user somehow
+	}
 }
 ?>
