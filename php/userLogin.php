930bb645783c51d19cf01a4172a0c37d3db32f76
Brandon Roberts <broberts@pugetsound.edu>
2015-03-04 02:15:30 -0800

    Adds password verification and tables currently used for the database

--- php/userLogin.php
+++ php/userLogin.php
@@ -3,7 +3,7 @@ require_once 'connect.php';
 
 if(isset($_POST['submit'])){ //if the Login button on index.html is set then do the logging in
 		LogIn($conn);
-	}
+}
 
 function LogIn($connection) {
 	//grab info from index.html 
@@ -18,10 +18,13 @@ function LogIn($connection) {
 	$sql = "SELECT pass FROM profile WHERE email = '$email '";//encrypted password of the user
 
 	$result = $connection->query($sql);
+	$row = $result->fetch_assoc();
 	
-	if($password){//FIX THE PASSWORD COMPARISON the user was indeed in the table and had the correct password
+	if($password == $row['pass'] ){//FIX THE PASSWORD COMPARISON the user was indeed in the table and had the correct password
 		setcookie($emailname , $email, time()+60*60*24, '/');
+		header('Location: http://glados/capstone/profile.html');
+	}else{
+		echo "Sorry you entered the wrong password";
 	}
-	header('Location: http://glados/capstone/profile.html');
 }
 ?>
