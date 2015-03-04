718279c75c53c807470e491200081e8f9915e96f
Brandon Roberts <broberts@pugetsound.edu>
2015-03-04 05:14:18 -0800

    Adds basic group page

--- php/addContact.php
+++ php/addContact.php
@@ -3,14 +3,19 @@ require_once 'connect.php';
 require_once 'functions.php';
 
 if(isset($_POST["contactEmail"])){
-	AddContact($conn);
+	if($_POST["contactEmail"] != "")
+		AddContact($conn);
+	header('Location: http://glados/capstone/profile.html');
 }
 
 function AddContact($connection){
 	$dateTime = new DateTime();
 	$result = $dateTime->format('Y-m-d H:i:s');
 	//insert my email, contacts email, and the current dateTime
+		//*********************Need to make sure the contactEmail is in the database
 	$sql = "INSERT INTO contacts (myEmail, contactEmail, dateConnected) VALUES ('$_COOKIE[email]', '$_POST[contactEmail]', '$result')";
+
+	
 	$result = $connection->query($sql);
 	header('Location: http://glados/capstone/profile.html');
 }
