0e3116c88c1b921d67c1d816c9e38150a4c73e31
Brandon Roberts <broberts@pugetsound.edu>
2015-03-05 02:14:45 -0800

    Adds creating group message that appears on the group page. Still working on navigating to different group pages from profile page

--- php/postMessage.php
+++ php/postMessage.php
@@ -4,7 +4,7 @@ require_once 'connect.php'; //connect to the database
 if(isset($_POST['message'])){ //if the user clicks the submit button on the groupPage
 	if($_POST["message"])
 		PostMessage($conn);
-//	header('Location: http://glados/capstone/group.html'); //go back to the group page without doing anything
+	header('Location: http://glados/capstone/group.html'); //go back to the group page without doing anything
 }
 
 function PostMessage($connection){
