5e68176fe5cff4b57f9d9a18eab6b8833e1abd93
nolderman <nolderman@pugetsound.edu>
2015-04-01 16:41:25 -0700

    test push

--- php/convSidebar.php
+++ php/convSidebar.php
@@ -2,16 +2,14 @@
 require_once 'connect.php';
 require_once 'functions.php';
 
-if(isset($_POST["uID"])){
-	GetConversations($conn);
-}
+GetConversations();
 
-function GetConversations($connection){
+function GetConversations(){
 	echo "<h1>Conversations</h1>";
 
 	$sql = "INSERT INTO contacts (myEmail, contactEmail, dateConnected) VALUES ('$_COOKIE[email]', '$_POST[contactEmail]', '$dateTime')"; //put the contact in the database
 
-	$result = $connection->query($sql);
+	$result = $conn->query($sql);
 	
 	header('Location: http://glados/capstone/profile.html');
 }
