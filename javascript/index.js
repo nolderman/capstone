ad44764e11b07f03899e9ecb87f87b8609d1d7d9
Brandon Roberts <broberts@pugetsound.edu>
2015-03-31 17:02:13 -0700

    Creates scrolling index page with fade in animations

--- javascript/index.js
+++ javascript/index.js
@@ -3,11 +3,32 @@ var controller;
 
 $(function() {
 	controller = new ScrollMagic.Controller();
-
+	
+	//tweens//							div, 		,time, {css params},	staggerTime 			
+	var missionFadeIn = TweenMax.staggerFrom(".missionContent", 2.5, {opacity:0});
+	var featuresFadeIn = TweenMax.staggerFrom(".featuresPanelContent", .5, {opacity:0}, 0.15);
+	var aboutUsFadeIn = TweenMax.staggerFrom(".teamMember", .5, {opacity:0}, 0.25);
+	
 	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"}) //				.addIndicators() // add indicators (requires plugin)
+	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200}) //				.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
-
+					
+	//mission, features, about us scenes 
+	var missionScene = new ScrollMagic.Scene({
+		triggerElement:".missionContent"
+		})
+	.setTween(missionFadeIn)
+	.addTo(controller)
+	.addIndicators();
+	
+	var featureScene = new ScrollMagic.Scene({triggerElement:".featuresContent"})
+	.setTween(featuresFadeIn)
+	.addTo(controller);
+	
+	var aboutUsScene = new ScrollMagic.Scene({triggerElement:".aboutus"})
+	.setTween(aboutUsFadeIn)
+	.addTo(controller);
+	
 	// change behaviour of controller to animate scroll instead of jump
 	controller.scrollTo(function (newpos) {
 		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
