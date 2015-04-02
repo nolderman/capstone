e0fe19644c63d8dcee99f925ec52cfd0effac4f8
nolderman <nolderman@pugetsound.edu>
2015-04-01 17:09:37 -0700

    fixes naming issue and continues edits towards database conversation access

--- php/convSidebar.php
+++ php/convSidebar.php
@@ -1,17 +1,19 @@
 <?php
-require_once 'connect.php';
-require_once 'functions.php';
+	require_once 'connect.php';
+	require_once 'functions.php';
 
-GetConversations();
+	$numConversations = 10;
 
-function GetConversations(){
 	echo "<h1>Conversations</h1>";
 
-	$sql = "INSERT INTO contacts (myEmail, contactEmail, dateConnected) VALUES ('$_COOKIE[email]', '$_POST[contactEmail]', '$dateTime')"; //put the contact in the database
+	$sql = "SELECT c_name FROM user NATURAL JOIN participates NATURAL JOIN conversation VALUES $_SESSION["uID"]"; //put the contact in the database
 
 	$result = $conn->query($sql);
-	
-	header('Location: http://glados/capstone/profile.html');
-}
+	$convNames = $result->fetch_array()
 
+	echo "<ul>"; //start an unordered list in html
+	for(int i = 0; i < $numConversations; i++){
+
+	}
+		
 ?>
