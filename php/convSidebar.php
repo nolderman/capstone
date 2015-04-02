9fecb409e8066d3be4d044776d449a71f00a2e64
nolderman <nolderman@pugetsound.edu>
2015-04-01 18:18:25 -0700

    deletes test files and merges test code into main code base

--- php/convSidebar.php
+++ php/convSidebar.php
@@ -2,17 +2,20 @@
 	require_once 'connect.php';
 	require_once 'functions.php';
 
+	$user = $_SESSION["uID"];
 	$numConversations = 10;
 
-	echo "<h1>Conversations</h1>";
 
-	$sql = "SELECT c_name FROM user NATURAL JOIN participates NATURAL JOIN conversation VALUES $_SESSION["uID"]"; //put the contact in the database
+	echo "<div class='sidebarHeader' align='center'>Conversations</div>";
+	
+	$sql = "SELECT c_name FROM user NATURAL JOIN participates NATURAL JOIN conversation WHERE uID = '$user'"; //put the contact in the database
 	$result = $conn->query($sql);
-	$convNames = $result->fetch_array()
+	$convNames = $result->fetch_array();
 
-	echo "<ul>"; //start an unordered list in html
-	for(int i = 0; i < $numConversations; i++){
-
+	//write out each conversation name to the sidebar
+	echo "<div class='sidebarContent' align='center'>";
+	for($i = 0; $i < $numConversations; $i++){
+		echo "<div class='convLink'>".$convNames[$i]."<br></div>"; //list out the conversation names
 	}
-	echo "</ul>"; //end the unordered list in html
+	echo "</div>";
 ?>
