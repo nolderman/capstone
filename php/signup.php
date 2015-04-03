00d88a56fa9ba55f0f4abe6f7cbce83e7b37074e
brobertsUPS <broberts@pugetsound.edu>
2015-04-03 02:59:45 -0700

    Fixes signup bug

--- php/signup.php
+++ php/signup.php
@@ -2,6 +2,10 @@
 require_once 'connect.php';
 require_once 'functions.php';
 
+if (session_status() == PHP_SESSION_NONE) { //we don't have a session already
+	session_start();
+}
+
 if(isset($_POST['submit'])){
 	SignUp($conn);
 }
@@ -56,11 +60,12 @@ function NewUser($connection){
 	$pass = sha1($password, $raw_output = false); //encrypt their password
 	$sql= "INSERT INTO user (uID,email,pass,picture,f_Name,m_name,l_Name,tag_visibility,profile_visible,block_invites,block_messages) VALUES ('NULL','$email','$pass','NULL','$firstName','NULL','$lastName','1','1','0','0')"; //make them a profile
 	$result = $connection->query($sql);
-
+	echo $email;
+	echo $pass;
 	
 	$uID =  mysqli_insert_id($connection); //get the id of the last inserted record
 	$uIDName = "uID";
-	//session_start();
+	
 	$_SESSION['uID'] = $uID;
 	//setcookie($uIDName, $uID, time()+60*60*24, '/');//set the user ID cookie for a day so we can get all of their information later
 	//header('Location: ../profile.php');		//log the user in
