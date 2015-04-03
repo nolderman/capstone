d81dcf6ba38c0ab61af654ed326c3180ac548db1
Eric <esculac@gmail.com>
2015-04-03 13:47:57 -0700

    Banner content now shares same top margin. Banner now pretty. Yay.

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
 					<h1>Capstone</h1>
 
 					<!--Login Form-->
-					<form name="login" class="loginform cf" accept-charset="utf-8" method= "POST" action="php/userLogin.php">  			       
+					<form name="login" class="content loginform cf" accept-charset="utf-8" method= "POST" action="php/userLogin.php">  			       
 						 <input type="email" id="email" name="usermail" placeholder="example@email.com" required>
 						 <input type="password"  id="password" name="password" placeholder="password" required>
 						 <input type="submit" class="submit hvr-fade-blue" name = "submit"  value="Login">
 					</form>   
 
-				<a href="register.php" class="signup hvr-fade"> Sign Up!</a>
+				<a href="register.php" class="content signup hvr-fade-green"> Sign Up!</a>
 			</div>	
 		</div>
 		
