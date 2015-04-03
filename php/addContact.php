40102c0e27c03e9553a915fb79932af2f2b667ea
Brandon Roberts <broberts@pugetsound.edu>
2015-04-03 01:08:56 -0700

    Fixes index page bug and updates addContact php

--- php/addContact.php
+++ php/addContact.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'connect.php';
 require_once 'functions.php';
-
-if(isset($_POST["contactEmail"])){
-	if($_POST["contactEmail"] != "") //only save a contact if the user put something in the submit box
+session_start(); //fetch the session variables from the database
+if(isset($_POST['contactEmail'])){
+	if(isset($_POST['contactEmail'])) //only save a contact if the user put something in the submit box
 		AddContact($conn);
 	header('Location: http://glados/capstone/profile.html');
 }
@@ -12,9 +12,16 @@ function AddContact($connection){
 	$dateTime = new DateTime();
 	$dateTime = $dateTime->format('Y-m-d H:i:s');
 	//insert my email, contacts email, and the current dateTime
-		//*********************Need to make sure the contactEmail is in the database before adding them
-	$sql = "INSERT INTO contacts (myEmail, contactEmail, dateConnected) VALUES ('$_COOKIE[email]', '$_POST[contactEmail]', '$dateTime')"; //put the contact in the database
-
+	$contactEmail = $_POST['contactEmail'];
+	$uID = $_SESSION['uID'];
+	
+	$getContactID = "SELECT uID FROM user WHERE email= '$contactEmail'";
+	$result = $connection->query($getContactID);
+	$row = fetch_array(MYSQLI_ASSOC);
+	$contactID = $row['uID'];
+	
+	//*********************Need to make sure the contactEmail is in the database before adding them
+	$sql = "INSERT INTO contacts (user, contact) VALUES ('$uID', '$contactID')"; //put the contact in the database
 	$result = $connection->query($sql);
 	
 	header('Location: http://glados/capstone/profile.html');
