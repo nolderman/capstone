5e5dcae16a3a70720efcfe8ee0b6b2cf22b4758d
nolderman <nolderman@pugetsound.edu>
2015-04-02 20:02:24 -0700

    test

--- php/connect.php
+++ php/connect.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'login.php';
 
-$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database); //connects to the database
+$conn = new mysqli($db_hostname, $db_username, $db_password, $db_database); //connects to the database
 
-if (mysqli_connect_errno()) {
+if ($conn->connect_error) {
   printf('Connect failed: %s\n', mysqli_connect_error());
   exit();
 }
