cb51e37019b2f42f7dda2428389f4a02273ccd4c
nolderman <nolderman@pugetsound.edu>
2015-04-03 00:43:59 -0700

    changes sidebar div positioning

--- php/convSidebar.php
+++ php/convSidebar.php
@@ -5,22 +5,19 @@
 	$user = $_SESSION["uID"];
 	$numConversations = 10;
 
-
 	echo "<div class='sidebarHeader'>Conversations</div>";
-	
 		$sql = "SELECT (cID, c_name) FROM (user NATURAL JOIN participates NATURAL JOIN conversation) WHERE (uID = '$user')"; //put the contact in the database
-		
-		if($result = $conn->query($sql)){
-			//write out each conversation name to the sidebar
-			echo "<div class='sidebarContent'>";
-			while($convos = $result->fetch_array(MYSQLI_ASSOC)){
-				echo "<a href = 'conversation.php?cID=".$convos['cid']."'>";
-				echo "<div class='convLink hvr-fade-green'>".$convos['c_name']."</div>";
-				echo "</a></br>";		
+		echo "<div class='sidebarContent'>";
+			if($result = $conn->query($sql)){
+				//write out each conversation name to the sidebar
+				while($convos = $result->fetch_array(MYSQLI_ASSOC)){
+					echo "<a href = 'conversation.php?cID=".$convos['cid']."'>";
+					echo "<div class='convLink hvr-fade-green'>".$convos['c_name']."</div>";
+					echo "</a></br>";		
+				}	
 			}
-			echo "</div>";
-		}
-		else{
-			echo "<div class='convLink'>You have no conversations!</div>";
-		}
+			else{
+				echo "<div class='convLink'>You have no conversations!</div>";
+			}
+		echo "</div>";
 ?>
--- php/groupSidebar.php
+++ php/groupSidebar.php
@@ -5,22 +5,19 @@
 	$user = $_SESSION['uID'];
 	$numConversations = 10;
 
-
 	echo "<div class='sidebarHeader'>Groups</div>";
-	
 		$sql = "SELECT (gID, g_name) FROM (user NATURAL JOIN members NATURAL JOIN groups) WHERE (uID ='$user')"; //put the contact in the database
-		
-		if($result = $conn->query($sql)){
-			//write out each group name to the sidebar and make them links
-			echo "<div class='sidebarContent'>";
-			while($groups = $result->fetch_assoc()){
-				echo "<a href = 'group.php?gID=".$groups['gid']."'>";
-				echo "<div class='groupLink'>".$groups['g_name']."</div>";
-				echo "</a></br>";
+		echo "<div class='sidebarContent'>";
+			if($result = $conn->query($sql)){
+				//write out each group name to the sidebar and make them links
+				while($groups = $result->fetch_assoc()){
+					echo "<a href = 'group.php?gID=".$groups['gid']."'>";
+					echo "<div class='groupLink'>".$groups['g_name']."</div>";
+					echo "</a></br>";
+				}
 			}
-			echo "</div>";
-		}
-		else{
-			echo "<div class='groupLink'>You have no groups!</div>";
-		}
+			else{
+				echo "<div class='groupLink'>You have no groups!</div>";
+			}
+		echo "</div>";
 ?>
