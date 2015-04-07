4869d4ab8ef0a3960cb4862e763fc081137410ee
nolderman <nolderman@pugetsound.edu>
2015-04-07 11:45:15 -0700

    fixes issue with tags so they properly populate again

--- php/tagUser.php
+++ php/tagUser.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'connect.php';
 require_once 'functions.php';
+require_once 'sessionStatus.php';
 
 if(isset($_POST["tagName"])){
 	CreateTag($connection);
@@ -9,14 +10,14 @@ if(isset($_POST["tagName"])){
 function CreateTag($connection){
 	//make a new tag
 	$tagName = $_POST["tagName"];
+	$user = $_SESSION["uID"];//doesn't have access to this variable because it is a separate php page
 
 	$insert_tag = "INSERT INTO tag (tag_name) VALUES ('$tagName')";
 	$connection->query($insert_tag);
 	
 	//attach this user with this tag
-	$insert_u_tagged = "INSERT INTO u_tagged (uID,tag_name) VALUES ('$uID','$tagName')";//set the creator to a moderator
+	$insert_u_tagged = "INSERT INTO u_tagged (uID,tag_name) VALUES ('$user','$tagName')";//set the creator to a moderator
 	$connection->query($insert_u_tagged);
-	//var_dump($connection);
 	header('Location: ../profile.php');
 }
 ?>
