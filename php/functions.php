c0e9f0ad2a2d06dd97d7090c4ccad1a7e93d6c8d
brobertsUPS <broberts@pugetsound.edu>
2015-05-10 21:45:28 -0700

    Updates functions

--- php/functions.php
+++ php/functions.php
@@ -743,7 +743,7 @@ function deleteGroupTag($connection, $gID, $tag_name)
 //-------------------------------------------------CHECK IF MODERATOR OF GROUP-------------------------------------------//
 function groupModCheck($connection, $uID, $gID)
 {
-    $sql    = "SELECT moderator FROM members WHERE uID = '$uID'";
+    $sql    = "SELECT moderator FROM members WHERE uID = '$uID' AND gID='$gID'";
     $result = $connection->query($sql);
     $row    = $result->fetch_array(MYSQLI_ASSOC); //get the array with uID and pass
     
